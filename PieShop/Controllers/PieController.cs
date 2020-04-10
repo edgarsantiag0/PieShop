317da7b25b71f64b497cf00972c161a56c64f06e
Edgar Santiago <itt.edgarstgo@gmail.com>
2020-04-10 14:31:43 -0400

    Adding navigation to the site

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -31,5 +31,14 @@ namespace PieShop.Controllers
             piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
